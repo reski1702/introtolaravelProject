dbca8c4493a2a4741f82c69ec68f61006215a751
reskidev <pesan.reski@gmail.com>
2020-05-20 22:04:38 +0700

    new

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Latihan Laravel Basic
                 </div>
 
                 <div class="links">
